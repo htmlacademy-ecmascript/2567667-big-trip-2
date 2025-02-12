86c80d3acd44fc66a2a225c29ccab699391fd74b
Amal <amal.gabidov@mail.ru>
2025-02-12 15:56:02 +0300

    add babel transpilation

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+
 module.exports = {
   entry: './src/main.js',
   output: {
@@ -13,4 +14,18 @@ module.exports = {
       patterns: [{ from: 'public' }],
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          },
+        },
+      },
+    ]
+  }
 };
