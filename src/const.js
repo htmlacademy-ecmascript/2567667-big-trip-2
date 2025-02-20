d12ef52326cb90caf9d4ec5aa31dab2f99d6399f
Amal <amal.gabidov@mail.ru>
2025-02-20 15:29:45 +0300

    add mockpoints in constants

--- src/const.js
+++ src/const.js
@@ -25,7 +25,7 @@ const DESTINATIONS = [
   },
   {
     id: 3,
-    name: 'Chamonix',
+    name: 'bfa5cb75-a1fe-4b77-a83c-0e528e910e06',
     description: 'Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.',
     pictures: [
       {
@@ -83,4 +83,57 @@ const OFFERS = [
   }
 ];
 
-export { POINT_TYPES, DESTINATIONS, OFFERS };
+const MOCK_POINTS = [
+  {
+    id: '0',
+    basePrice: 1100,
+    dateFrom: '2019-07-10T22:55',
+    dateTo: '2019-07-11T11:22',
+    destination: [2],
+    isFavorite: false,
+    offers: [2],
+    type: 'taxi'
+  },
+  {
+    id: '1',
+    basePrice: 3470,
+    dateFrom: '2019-03-18T12:25',
+    dateTo: '2019-03-18T13:35',
+    destination: [2],
+    isFavorite: false,
+    offers: [1, 2],
+    type: 'flight'
+  },
+  {
+    id: '2',
+    basePrice: 1000,
+    dateFrom: '2019-03-18T14:30',
+    dateTo: '2019-03-18T16:05',
+    destination: [1],
+    isFavorite: false,
+    offers: [1],
+    type: 'drive'
+  },
+  {
+    id: '3',
+    basePrice: 1500,
+    dateFrom: '2019-03-18T12:25',
+    dateTo: '2019-03-18T13:35',
+    destination: [3],
+    isFavorite: false,
+    offers: '',
+    type: 'drive'
+  },
+  {
+    id: '4',
+    basePrice: 6000,
+    dateFrom: '2019-03-19T11:20',
+    dateTo: '2019-03-19T13:00',
+    destination: [1],
+    isFavorite: false,
+    offers: [1],
+    type: 'check-in'
+  }
+];
+
+export { POINT_TYPES, DESTINATIONS, OFFERS, MOCK_POINTS };
