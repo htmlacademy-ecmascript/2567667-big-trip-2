5b627dfb46054deb21bf2f89ad9f82277ccadfe1
Amal <amal.gabidov@mail.ru>
2025-03-23 13:02:01 +0300

    edit form with dynamic type and destination updates

--- src/view/edit-event-form-view.js
+++ src/view/edit-event-form-view.js
@@ -1,12 +1,29 @@
-import AbstractView from '../framework/view/abstract-view.js';
+import AbstractStatefulView from '../framework/view/abstract-stateful-view.js';
 import dayjs from 'dayjs';
 
-function createEditEventFormTemplate(point, offers, destinations) {
-  const { id, dateFrom, dateTo, destination, type, basePrice } = point;
+const EVENT_TYPES = ['taxi', 'bus', 'train', 'ship', 'drive', 'flight', 'check-in', 'sightseeing', 'restaurant'];
+
+function createEventTypeListTemplate(type, id) {
+  return `
+    <div class="event__type-list">
+      <fieldset class="event__type-group">
+        <legend class="visually-hidden">Event type</legend>
+        ${EVENT_TYPES.map((eventType) => `
+          <div class="event__type-item">
+            <input id="event-type-${eventType}-${id}" class="event__type-input visually-hidden" type="radio" name="event-type" value="${eventType}" ${eventType === type ? 'checked' : ''}>
+            <label class="event__type-label event__type-label--${eventType}" for="event-type-${eventType}-${id}">${eventType}</label>
+          </div>
+        `).join('')}
+      </fieldset>
+    </div>
+  `;
+}
+
+function createEditEventFormTemplate(state, destinations) {
+  const { id, dateFrom, dateTo, destination, type, basePrice, availableOffers } = state;
   const formattedDateFrom = dayjs(dateFrom).format('DD/MM/YY HH:mm');
   const formattedDateTo = dayjs(dateTo).format('DD/MM/YY HH:mm');
-  const destinationData = destinations.find((dest) => dest.id === destination) || {};
-  const availableOffers = offers.find((offer) => offer.type === type)?.offers || [];
+  const destinationData = destinations.find((dest) => dest.id === destination) || { name: '', description: '', pictures: [] };
 
   function createOffersTemplate() {
     return availableOffers.length > 0
@@ -28,16 +45,17 @@ function createEditEventFormTemplate(point, offers, destinations) {
       <form class="event event--edit" action="#" method="post">
         <header class="event__header">
           <div class="event__type-wrapper">
-            <label class="event__type  event__type-btn" for="event-type-toggle-${id}">
+            <label class="event__type event__type-btn" for="event-type-toggle-${id}">
               <span class="visually-hidden">Choose event type</span>
               <img class="event__type-icon" width="17" height="17" src="img/icons/${type}.png" alt="Event type icon">
             </label>
             <input class="event__type-toggle visually-hidden" id="event-type-toggle-${id}" type="checkbox">
+            ${createEventTypeListTemplate(type, id)}
           </div>
 
           <div class="event__field-group event__field-group--destination">
             <label class="event__label event__type-output" for="event-destination-${id}">${type}</label>
-            <input class="event__input event__input--destination" id="event-destination-${id}" type="text" name="event-destination" value="${destinationData.name || ''}" list="destination-list-${id}">
+            <input class="event__input event__input--destination" id="event-destination-${id}" type="text" name="event-destination" value="${destinationData.name}" list="destination-list-${id}">
             <datalist id="destination-list-${id}">
               ${destinations.map((dest) => `<option value="${dest.name}"></option>`).join('')}
             </datalist>
@@ -69,10 +87,10 @@ function createEditEventFormTemplate(point, offers, destinations) {
 
           <section class="event__section event__section--destination">
             <h3 class="event__section-title event__section-title--destination">Destination</h3>
-            <p class="event__destination-description">${destinationData.description || 'No description available'}</p>
+            <p class="event__destination-description">${destinationData.description}</p>
             <div class="event__photos-container">
               <div class="event__photos-tape">
-                ${(destinationData.pictures || []).map((pic) => `<img class="event__photo" src="${pic.src}" alt="Event photo">`).join('')}
+                ${destinationData.pictures.map((pic) => `<img class="event__photo" src="${pic.src}" alt="Event photo">`).join('')}
               </div>
             </div>
           </section>
@@ -81,8 +99,8 @@ function createEditEventFormTemplate(point, offers, destinations) {
     </li>
   `;
 }
-export default class EditEventFormView extends AbstractView {
-  #point = null;
+
+export default class EditEventFormView extends AbstractStatefulView {
   #offers = null;
   #destinations = null;
   #onFormSubmit = null;
@@ -90,26 +108,48 @@ export default class EditEventFormView extends AbstractView {
 
   constructor({ point, offers, destinations, onFormSubmit, onEditClose }) {
     super();
-    this.#point = point;
     this.#offers = offers;
     this.#destinations = destinations;
     this.#onFormSubmit = onFormSubmit;
     this.#onEditClose = onEditClose;
 
-    this.element.querySelector('form')
-      .addEventListener('submit', this.#handleFormSubmit);
+    const availableOffers = offers.find((o) => o.type === point.type)?.offers || [];
 
-    this.element.querySelector('.event__rollup-btn')
-      .addEventListener('click', this.#handleEditClose);
+    this._setState({
+      ...EditEventFormView.parsePointToState(point),
+      availableOffers
+    });
+
+    this._restoreHandlers();
   }
 
   get template() {
-    return createEditEventFormTemplate(this.#point, this.#offers, this.#destinations);
+    return createEditEventFormTemplate(this._state, this.#destinations);
+  }
+
+  static parsePointToState(point) {
+    return {
+      ...point
+    };
+  }
+
+  static parseStateToPoint(state) {
+    const point = { ...state };
+    delete point.availableOffers;
+    return point;
+  }
+
+  _restoreHandlers() {
+    this.element.querySelector('form').addEventListener('submit', this.#handleFormSubmit);
+    this.element.querySelector('.event__rollup-btn').addEventListener('click', this.#handleEditClose);
+    this.element.querySelector('.event__type-group').addEventListener('change', this.#handleTypeChange);
+    this.element.querySelector('.event__input--destination').addEventListener('change', this.#handleDestinationChange);
+    this.element.querySelector('.event__input--price').addEventListener('change', this.#handlePriceChange);
   }
 
   #handleFormSubmit = (evt) => {
     evt.preventDefault();
-    this.#onFormSubmit();
+    this.#onFormSubmit(EditEventFormView.parseStateToPoint(this._state));
   };
 
   #handleEditClose = (evt) => {
@@ -117,5 +157,26 @@ export default class EditEventFormView extends AbstractView {
     if (this.#onEditClose) {
       this.#onEditClose();
     }
+  };
+
+  #handleTypeChange = (evt) => {
+    const newType = evt.target.value;
+    const newOffers = this.#offers.find((o) => o.type === newType)?.offers || [];
+
+    this.updateElement({
+      type: newType,
+      availableOffers: newOffers
+    });
+  };
+
+  #handleDestinationChange = (evt) => {
+    const selectedDest = this.#destinations.find((d) => d.name === evt.target.value);
+    if (selectedDest) {
+      this.updateElement({ destination: selectedDest.id });
+    }
+  };
+
+  #handlePriceChange = (evt) => {
+    this.updateElement({ basePrice: Number(evt.target.value) });
   };
 }
